86b1cf3440862b2c7841ada6f67d6c13acdc49c4
Daniel Diego Wildin <ddiegow@uoc.edu>
2023-06-21 11:47:19 +0900

    create draw func

--- gol/gol.go
+++ gol/gol.go
@@ -1,7 +1,9 @@
 package gol
 
-// GAME OF LIFE - SERIAL IMPLEMENTATION
+import "fmt"
 
+// GAME OF LIFE - SERIAL IMPLEMENTATION
+// TODO: check func not working correctly
 type position struct {
 	x int
 	y int
@@ -34,20 +36,42 @@ func (b *board) init(size int) {
 	b.b = make([][]cell, b.n)
 	for i := range b.b {
 		b.b[i] = make([]cell, b.n)
+		for j := range b.b[i] {
+			b.b[i][j].pos.init(j, i)
+		}
 	}
 	b.left = -1
 	b.top = size
 	b.right = size
 	b.bottom = -1
 }
+func (b *board) activate(p position) {
+	c := b.getCell(p)
+	if c == nil {
+		return
+	}
+	c.current = true
+}
+
+/*
+Check if position is withing the limits of the board
+*/
 func (b *board) withinLimits(pos position) bool {
 	return pos.x > b.left && pos.x < b.right && pos.y < b.top && pos.y > b.bottom
 }
-func getCell(board [][]cell, position2 position) cell {
 
+/*
+Return pointer to cell based on position. Will return nil if position is not within the board limits.
+*/
+func (b *board) getCell(p position) *cell {
+	if !b.withinLimits(p) {
+		return nil
+	}
+	return &b.b[p.y][p.x]
 }
-func run() {
 
+func (b *board) countAround(c cell) int {
+	totalCount := 0
 	around := []position{
 		{-1, 0},
 		{-1, 1},
@@ -58,5 +82,61 @@ func run() {
 		{0, -1},
 		{-1, -1},
 	}
+	for i := range around {
+		a := b.getCell(c.pos.add(around[i]))
+		if a == nil {
+			continue
+		}
+		if a.current == true {
+			totalCount++
+		}
+	}
+	return totalCount
+}
 
+func (b *board) check() {
+	for i := range b.b {
+		for j := range b.b[i] {
+			count := b.countAround(b.b[i][j])
+			if count >= 2 || count < 4 {
+				b.b[i][j].next = true
+			} else {
+				b.b[i][j].next = false
+			}
+		}
+	}
+}
+func (b *board) update() {
+	for i := range b.b {
+		for j := range b.b[i] {
+			b.b[i][j].current = b.b[i][j].next
+		}
+	}
+}
+
+func (b *board) draw() {
+	for i := b.n - 1; i >= 0; i-- {
+		fmt.Printf("|")
+		for j := range b.b[i] {
+			if b.b[i][j].current {
+				fmt.Printf(" x |")
+			} else {
+				fmt.Printf("   |")
+			}
+
+		}
+		fmt.Printf("\n")
+
+	}
+}
+func Run() {
+	var b board
+	b.init(3)
+	b.activate(position{0, 1})
+	b.activate(position{1, 1})
+	b.activate(position{2, 1})
+	b.draw()
+	b.check()
+	b.update()
+	b.draw()
 }
--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"playground/sieve"
-	"time"
+	"playground/gol"
 )
 
 func main() {
-	//playing.PlayWithChans()
-	//fmt.Println(fibo.Fibonacci(50))
-	//capitalize.Capitalize([]string{"Hello world", "How are you doing"})
-	//input := []string{"apple", "bakery", "ladder", "zebra", "Akira", "left", "jail", "Mizuho", "Game", "Lost", "Addendum", "paradox", "cap", "destruction", "studio", "urine", "release", "bin", "flawed", "lose", "dish", "path", "cultivate", "charity", "abridge", "keep", "skip", "stun", "navy", "rich", "computer", "fool", "wait", "remain", "irony", "contraction", "braid", "skeleton", "sequence", "monstrous", "avenue", "recruit", "socialist", "head", "hill", "button", "cattle", "self", "pop", "miner", "bargain", "advocate", "identity", "package", "slot", "question", "extraterrestrial", "domination", "sheet", "establish", "voter"}
-	//fmt.Println(mergesort.Sort(input))
-	start := time.Now()
-	//sieve.Run() // THIS TAKES FOREVER WITH LARGE NUMBERS
-	fmt.Printf("The function took %d to run\n", time.Since(start))
-	start = time.Now()
-	sieve.ConcurrentSieve()
-	fmt.Printf("The function took %d to run\n", time.Since(start))
+
+	gol.Run()
 }
